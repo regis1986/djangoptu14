5396307aaec19abb6cc821562ab0dcacdc76ed18
Regis <r.petrauskui@gmail.com>
2023-08-23 15:16:55 +0300

    paskaita 4, static settingai

--- library/views.py
+++ library/views.py
@@ -6,10 +6,14 @@ from .models import Book, Author, Genre, BookInstance
 def index(request):
     num_boks = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+    num_authors = Author.objects.count()
 
     context_t = {
         'num_books_t': num_boks,
-        'num_instances_t': num_instances
+        'num_instances_t': num_instances,
+        'num_instances_available_t': num_instances_available,
+        'num_authors_t': num_authors
     }
 
     return render(request, 'index.html', context=context_t)
