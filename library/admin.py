9e58fefe199680b3d63b8d037727a266f8aedf84
Regis <r.petrauskui@gmail.com>
2023-08-23 10:39:54 +0300

    paskaita 3, AuthorAdmin

--- library/admin.py
+++ library/admin.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 from . models import Author, Book, Genre, BookInstance
 
 
+class BooksInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInLine]
 
 
 @admin.register(BookInstance)
@@ -18,5 +24,10 @@ class BookInstanceAdmin(admin.ModelAdmin):
         ('Availability', {'fields': ('status', 'due_back')}),
     )
 
-admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_books')
+
+
 admin.site.register(Genre)
--- library/models.py
+++ library/models.py
@@ -12,12 +12,18 @@ class Author(models.Model):
     def __str__(self):
         return f'{self.last_name}  {self.first_name}'
 
+    def display_books(self):
+        return ', '.join(book.title for book in self.books.all()[:3])
+
+    display_books.short_Description = 'Knygos'
+
+
 class Book(models.Model):
     title = models.CharField('Pavadinimas', max_length=200)
     summary = models.TextField('Aprašymas', max_length=1000)
     isbn = models.CharField('ISBN', max_length=13,
                             help_text='13 Simbolių <a href="https://lt.wikipedia.org/wiki/ISBN"> ISBN kodas</a>')
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, related_name='books')
     genre = models.ManyToManyField('Genre', help_text='Išsirinkite žanrą(us)')
 
     def __str__(self):
