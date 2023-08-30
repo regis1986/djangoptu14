75c709bddc7b75157f352bc52fe33cccf53991c3
Regis <r.petrauskui@gmail.com>
2023-08-30 10:35:28 +0300

    base su linkais, media folderio prijungimas, paveikslelio prijungimas prie modelio

--- djangoptu14/urls.py
+++ djangoptu14/urls.py
@@ -22,4 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('library/', include('library.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  \
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
